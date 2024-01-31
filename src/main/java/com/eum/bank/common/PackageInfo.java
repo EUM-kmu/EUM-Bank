070346a9ab566ad514ba4058e53d304dd2dd6425
yeonsu <jack0810@kookmin.ac.kr>
2024-01-31 15:07:00 +0900

    fix code style

--- src/main/java/com/eum/bank/common/PackageInfo.java
+++ src/main/java/com/eum/bank/common/PackageInfo.java
@@ -1,14 +1,13 @@
 package com.eum.bank.common;
 
 public final class PackageInfo {
-
-    private PackageInfo() {
-    }
-
     // 계좌 번호 길이
     public static final int ACCOUNT_NUMBER_LENGTH = 12;
     // 송금 타입 자유
     public static final String FREE_TYPE = "free";
 
+    private PackageInfo() {
+    
+    }
 
 }
