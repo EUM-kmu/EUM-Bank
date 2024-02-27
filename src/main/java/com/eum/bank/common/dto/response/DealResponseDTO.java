4363af51b245e220be34f7c4e1d30047136156d4
jykim-rust <alexj99@naver.com>
2024-02-27 19:27:10 +0900

    :sparkles: 거래관련 api의 반환타입을 좀 더 구체적이게 변경

--- src/main/java/com/eum/bank/common/dto/response/DealResponseDTO.java
+++ src/main/java/com/eum/bank/common/dto/response/DealResponseDTO.java
@@ -13,5 +13,17 @@ public class DealResponseDTO {
     public static class createDeal {
         // 거래 id
         private Long dealId;
+        // 송신자 계좌 번호
+        private String senderAccountNumber;
+        // 상태
+        private String status;
+        // 예치금
+        private Long deposit;
+        // 최대 인원수
+        private Long maxPeopleNum;
+        // 실제 모집인원수
+        private Long realPeopleNum;
+        // 게시글 ID
+        private Long postId;
     }
 }
